9ae1805272fe4d152c0f9cfd34383c58c6e5f606
Nada El Ouarghi <nada.elouarghi@etudiant.univ-brest.fr>
2024-11-24 10:52:32 +0100

    test notif en vercel 2

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,5 @@
 // service-worker.js
+
 if( 'function' === typeof importScripts) {
   // eslint-disable no-undef 
   importScripts("https://js.pusher.com/beams/service-worker.js");
@@ -20,50 +21,10 @@ self.addEventListener("notificationclick", function (event) {
 });
 
 
-
+/*
 self.addEventListener("notificationclick", function (event) {
     event.notification.close();
     event.waitUntil(clients.openWindow(event.notification.data.url));
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*proper initialization
-if( 'function' === typeof importScripts) {
-  /* eslint-disable no-undef 
-  importScripts("https://js.pusher.com/beams/service-worker.js");
-}
-
-PusherPushNotifications.onNotificationReceived = ({
-  pushEvent,
-  payload,
-  handleNotification,
-}) => {
-
-  console.log("worker got : " + JSON.stringify(payload));
-
-  // Get the client.
-  self.clients.matchAll().then((matchedClient) => matchedClient.forEach(client => {
-    client.postMessage(payload.data);
-  }));
-
-  // Your custom notification handling logic here 🛠️
-  // This method triggers the default notification handling logic offered by
-  // the Beams SDK. This gives you an opportunity to modify the payload.
-  // E.g. payload.notification.title = "A client-determined title!"
-  pushEvent.waitUntil(handleNotification(payload));
-};
 */
--- src/index.js
+++ src/index.js
@@ -12,7 +12,22 @@ root.render(
     <App />
   </React.StrictMode>
 );
-if ("serviceWorker" in navigator) {
+
+
+
+// Enregistrer le service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/service-worker.js')
+    .then(function (registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(function (error) {
+      console.error('Service Worker registration failed:', error);
+    });
+}
+
+/*if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
         navigator.serviceWorker
             .register("/service-worker.js")
@@ -23,7 +38,7 @@ if ("serviceWorker" in navigator) {
                 console.error("Service Worker registration failed:", error);
             });
     });
-}
+}*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
